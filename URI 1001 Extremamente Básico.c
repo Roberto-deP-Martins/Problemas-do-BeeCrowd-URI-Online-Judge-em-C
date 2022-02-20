9303a108808db6ad11b029837910530734c7179b
Roberto Martins <roberto.martinsps3@gmail.com>
2022-02-20 14:47:51 -0300

    Add files via upload

---
+++ URI 1001 Extremamente Básico.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    int a;
+    int b;
+    scanf("%i", &a);
+    scanf("%i", &b);
+    int resultado = a + b;
+    printf("X = %i\n", resultado);
+
+    return 0;
+}
