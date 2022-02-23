180b45a1351f4b3c7bbeda2a5198d7d3b0c2125c
Roberto Martins <roberto.martinsps3@gmail.com>
2022-02-23 18:44:09 -0300

    URI 1003

---
+++ URI_1003_Soma_Simples.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int a;
+    scanf("%i", &a);
+    int b;
+    scanf("%i", &b);
+    int soma = a + b;
+    printf("SOMA = %i\n", soma);
+    
+    return 0;
+}
