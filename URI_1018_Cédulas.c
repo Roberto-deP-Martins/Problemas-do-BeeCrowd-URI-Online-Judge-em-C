97bc91ee69a38d5252664f7af9efeb98f524c62a
Roberto Martins <robertomartins@id.uff.br>
2022-04-05 02:15:04 -0300

    Reduz código de 1018 usando loop for

--- URI_1018_Cédulas.c
+++ URI_1018_Cédulas.c
@@ -1,36 +1,20 @@
 #include <stdio.h>
 
 int main() {
-    int valor, qtd;
+    int valor, qtd, divisor;
     scanf("%i", &valor);
     printf("%i\n", valor);
-    qtd = valor / 100;
-    printf("%i nota(s) de R$ 100,00\n", qtd);
-    valor = valor % 100;
-
-    qtd = valor / 50;
-    printf("%i nota(s) de R$ 50,00\n", qtd);
-    valor = valor % 50;
-
-    qtd = valor / 20;
-    printf("%i nota(s) de R$ 20,00\n", qtd);
-    valor = valor % 20;
-
-    qtd = valor / 10;
-    printf("%i nota(s) de R$ 10,00\n", qtd);
-    valor = valor % 10;
-
-    qtd = valor / 5;
-    printf("%i nota(s) de R$ 5,00\n", qtd);
-    valor = valor % 5;
-
-    qtd = valor / 2;
-    printf("%i nota(s) de R$ 2,00\n", qtd);
-    valor = valor % 2;
-
-    qtd = valor / 1;
-    printf("%i nota(s) de R$ 1,00\n", qtd);
-    valor = valor % 1;
+    for (divisor = 100; divisor > 0;){
+        qtd = valor / divisor;
+        printf("%i nota(s) de R$ %i,00\n", qtd, divisor);
+        valor = valor % divisor;
+        if (divisor != 50) {
+            divisor = divisor / 2;
+        }
+        else {
+            divisor = 20;
+        }
+    }
 
     return 0;
 }
