6deb231ac68a47f3d75418f44bbb0cb59d538270
Roberto Martins <robertomartins@id.uff.br>
2022-04-05 22:47:40 -0300

    Reduz código usando loop for ao invés de ifs

--- URI_1021_Notas_e_Moedas.c
+++ URI_1021_Notas_e_Moedas.c
@@ -2,57 +2,39 @@
 
 int main() {
 
-    float valor_temp;
-    int qtd, valor;
-    scanf("%f", &valor_temp);
+    double valor_temp, divisor_formatado;
+    int qtd, valor, divisor;
+    scanf("%lf", &valor_temp);
     valor = valor_temp * 100;
     printf("NOTAS:\n");
-    qtd = valor / 10000;
-    printf("%i nota(s) de R$ 100.00\n", qtd);
-    valor = valor % 10000;
-
-    qtd = valor / 5000;
-    printf("%i nota(s) de R$ 50.00\n", qtd);
-    valor = valor % 5000;
-
-    qtd = valor / 2000;
-    printf("%i nota(s) de R$ 20.00\n", qtd);
-    valor = valor % 2000;
-
-    qtd = valor / 1000;
-    printf("%i nota(s) de R$ 10.00\n", qtd);
-    valor = valor % 1000;
-
-    qtd = valor / 500;
-    printf("%i nota(s) de R$ 5.00\n", qtd);
-    valor = valor % 500;
-
-    qtd = valor / 200;
-    printf("%i nota(s) de R$ 2.00\n", qtd);
-    valor = valor % 200;
-
-    printf("MOEDAS:\n");
-    qtd = valor / 100;
-    printf("%i moeda(s) de R$ 1.00\n", qtd);
-    valor = valor % 100;
+    for (divisor = 10000; divisor > 0;) {
+        qtd = valor / divisor;
+        divisor_formatado = divisor / 100.0;
+        if (divisor >= 200) { // 200 representa 200 centavos, ou seja, 2 reais, menor valor ainda em nota.
+            printf("%d nota(s) de R$ %.2lf\n", qtd, divisor_formatado);
+        }
+        else { // Abaixo de 200 centavos, ou seja, 1 real ou menos, usa-se moeads.
+            if (divisor == 100) { // Mostra que passa-se a mostrar moeadas a partir do maior valor de moeda
+                printf("MOEDAS:\n");
+            }
+            printf("%d moeda(s) de R$ %.2lf\n", qtd, divisor_formatado);
+        }
+        valor = valor % divisor; // valor remanescente a ser dividido em notas e moedas
+        // Altera o valor da variável do loop for
+        /* 50 reais, 5 reais e 25 centavos são os três únicos valores cujo próximo valor em ordem decrescente é
+        dado por uma divisão por 2.5 */
+        if (divisor == 5000 || divisor == 500 || divisor == 25) {
+            divisor /= 2.5;
+        }
+        /* 5 centavos é o único valor cujo próximo valor em ordem decrescente é dado por divisão de 5 */
+        else if (divisor == 5)
+        {
+            divisor = 1;
+        }
+        else { // Todos os outros casos tem o valor seguinte dado por uma divisão por 2
+            divisor /= 2;
+        }
+    }
     
-    qtd = valor / 50;
-    printf("%i moeda(s) de R$ 0.50\n", qtd);
-    valor = valor % 50;
-
-    qtd = valor / 25;
-    printf("%i moeda(s) de R$ 0.25\n", qtd);
-    valor = valor % 25;
-
-    qtd = valor / 10;
-    printf("%i moeda(s) de R$ 0.10\n", qtd);
-    valor = valor % 10;
-    
-    qtd = valor / 5;
-    printf("%i moeda(s) de R$ 0.05\n", qtd);
-    valor = valor % 5;
-
-    qtd = valor / 1;
-    printf("%i moeda(s) de R$ 0.01\n", qtd);
     return 0;
 }
